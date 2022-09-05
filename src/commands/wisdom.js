6413829f3652c2f9846561821f198dea7c3476d8
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2022-09-05 12:50:47 -0400

    feat: fortune cookie command

---
+++ src/commands/wisdom.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getFortune } = require('../models/freeignorance');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('wisdom')
+		.setDescription('Get a little wisdom.'),
+	async execute(interaction) {
+		await getFortune().then(async (response) => {
+			const message= `${response}`;
+			return interaction.reply(message);
+		});
+		return;
+	},
+}
