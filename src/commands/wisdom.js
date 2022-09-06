75e8b9496ac7a87e04fb72bde3d959c893a058ed
Beau Bouchard <bnb2011@gmail.com>
2022-09-06 14:50:06 -0400

    fix: linted

--- src/commands/wisdom.js
+++ src/commands/wisdom.js
@@ -1,15 +1,15 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { getFortune } = require('../models/freeignorance');
+const { SlashCommandBuilder } = require("discord.js");
+const { getFortune } = require("../models/freeignorance");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('wisdom')
-		.setDescription('Get a little wisdom.'),
-	async execute(interaction) {
-		await getFortune().then(async (response) => {
-			const message= `${response}`;
-			return interaction.reply(message);
-		});
-		return;
-	},
-}
+  data: new SlashCommandBuilder()
+    .setName("wisdom")
+    .setDescription("Get a little wisdom."),
+  async execute(interaction) {
+    await getFortune().then(async (response) => {
+      const message = `${response}`;
+      return interaction.reply(message);
+    });
+    return;
+  },
+};
